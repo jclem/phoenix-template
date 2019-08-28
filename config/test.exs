3afa27230775c78b2e2deeb47d282fb0fc0f30e7
Jonathan Clem <jclem@github.com>
2019-08-28 17:50:29 -0400

    Add a test workflow

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,16 @@
 use Mix.Config
 
-# Configure your database
 config :app, App.Repo,
   database: "app_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure your database
+if System.get_env("GITHUB_ACTIONS") do
+  config :app, App.Repo,
+    username: "postgres",
+    password: "postgres"
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
