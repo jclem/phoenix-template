9d46605d8c70e6c17c71ead923079c6ef71eb2b3
Jonathan Clem <jonathan@jclem.net>
2020-07-08 14:54:14 -0400

    Update npm package versions

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -12,16 +12,16 @@ module.exports = (env, options) => {
     optimization: {
       minimizer: [
         new TerserPlugin({cache: true, parallel: true, sourceMap: devMode}),
-        new OptimizeCSSAssetsPlugin({}),
-      ],
+        new OptimizeCSSAssetsPlugin({})
+      ]
     },
     entry: {
-      app: glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
+      app: glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
     },
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, '../priv/static/js'),
-      publicPath: '/js/',
+      publicPath: '/js/'
     },
     devtool: devMode ? 'source-map' : undefined,
     module: {
@@ -30,18 +30,18 @@ module.exports = (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
-          },
+            loader: 'babel-loader'
+          }
         },
         {
           test: /\.[s]?css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-        },
-      ],
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        }
+      ]
     },
     plugins: [
       new MiniCssExtractPlugin({filename: '../css/app.css'}),
-      new CopyWebpackPlugin([{from: 'static/', to: '../'}]),
-    ],
+      new CopyWebpackPlugin({patterns: [{from: 'static/', to: '../'}]})
+    ]
   }
 }
