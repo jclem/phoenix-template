24c8db258dfe0fd7f554751c35df55b256d36304
Jonathan Clem <jonathan@jclem.net>
2020-09-18 12:07:43 -0400

    Add postcss-nested

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-nested'),
     require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+    require('autoprefixer')
+  ]
 }
